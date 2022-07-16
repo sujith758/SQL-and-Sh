651f67ea17e7f5e27f9eb4af23c687f5bd8ef5a9
sujith758 <54630564+sujith758@users.noreply.github.com>
2022-07-16 12:16:19 -0500

    Update Data Validation.sh

--- Data Validation.sh
+++ Data Validation.sh
@@ -2,6 +2,6 @@ FILEPATH=$1
 
 export PGPASSWORD=postgres
 psql -h localhost -p 5432 -d cyclecrash -U postgres -f Data Validation.sql
-List_of_reasons=`psql -h localhost -p 5432 -d cyclecrash -U postgres -t -q -c 'select crashdate FROM crash_data.cyclecrashfreq1 WHERE DATE != ('YYYY-MM-DD');
+Vdate=`psql -h localhost -p 5432 -d cyclecrash -U postgres -t -q -c 'select crashdate FROM crash_data.cyclecrashfreq1 WHERE DATE != ('YYYY-MM-DD');
 '`
-echo "Reasons: $List_of_reasons"
+echo "Valid Dates: $Vdate"
